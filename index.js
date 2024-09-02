f2262513e5659d0ac71d70698011299d045d5818
Rehab <88044583+rehabnaf@users.noreply.github.com>
2024-09-02 13:39:17 +0530

    Create grids based on user input

--- index.js
+++ index.js
@@ -7,10 +7,42 @@ for (let i = 1; i <= 16; ++i) {
         container.appendChild(square);
     }
 }
+let squares = document.querySelectorAll("#container div")
 
-const squares = document.querySelectorAll("#container div")
-squares.forEach((square) => {
-    square.addEventListener("mouseover", () => {
-        square.style.backgroundColor = "yellow";
-    });
+function addHoverEffect() {
+    squares.forEach((square) => {
+        square.addEventListener("mouseover", () => {
+            square.style.backgroundColor = "yellow";
+        });
+    })
+}
+
+addHoverEffect();
+
+const inputBtn = document.querySelector("#user-input");
+
+inputBtn.addEventListener("click", function () {
+    const numOfSquares = prompt("Enter the number of squares per side? ")
+    removeSquares();
+    addSquares(numOfSquares);
 })
+
+function removeSquares() {
+    squares.forEach((square) => {
+        container.removeChild(square);
+    })
+}
+
+function addSquares(numOfSquares) {
+    for (let i = 1; i <= numOfSquares; ++i) {
+        for (let j = 1; j <= numOfSquares; ++j) {
+            const square = document.createElement("div");
+            square.style.width = `calc(700px/${numOfSquares})`;
+            square.style.height = `calc(700px/${numOfSquares})`;
+            container.appendChild(square);
+        }
+    }
+    squares = document.querySelectorAll("#container div");
+    addHoverEffect();
+}
+
